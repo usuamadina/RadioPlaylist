8149aff2166b4ba219765f4aac396af81b7c9ae2
umadina <u.madina.i@gmail.com>
2018-03-25 12:57:32 +0200

    Populate genresTable

--- radioPlaylists/GenresTableViewController.swift
+++ radioPlaylists/GenresTableViewController.swift
@@ -8,16 +8,100 @@
 
 import UIKit
 
-class GenresTableViewController: UITableViewController {
+ var selectedCategory: String = ""
+
+class GenresTableViewController: UITableViewController{
+    
+    var categoryNameList = Array<String>()
+    var list:[GenderStruct] = [GenderStruct]()
+   
+    struct GenderStruct
+    {
+        var imgUrl = ""
+        var genreName = ""
+        
+        init(imgUrl:String, genreName:String)
+        {
+            self.imgUrl = imgUrl
+            self.genreName = genreName
+        }
+    }
+    var genres = NSDictionary()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        let url = URL(string: "https://ia800505.us.archive.org/4/items/03.InternetRadioStationJson/03.InternetRadioStationJson.txt")!
+      
+        
+        // let request = NSMutableURLRequest(url:url)
+        
+        let task = URLSession.shared.dataTask(with: url)
+        {
+            (data, response, error) in
+            if error != nil
+            {
+                print(error as Any)
+            }
+            else{
+                if let content = data
+                {
+                    do
+                    {
+                        let jsonResult = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                       // print(jsonResult)
+                        
+                        if let categories = (jsonResult["categories"] as? NSDictionary)
+                        {
+                            DispatchQueue.main.sync(execute:{
+                                
+                                for (categoryName, categoryContent) in categories
+                                {
+                                    if categoryName is String
+                                    {
+                                        self.categoryNameList.append(categoryName as! String)
+                                        
+                                        if let content = categoryContent as? NSDictionary
+                                        {
+                                            
+                                            self.list.append(GenderStruct(imgUrl: content["imageUrl"] as! String, genreName: content["name"] as! String))
+                                            // print(content["imageUrl"] as! String)
+                                            
+                                        }
+                                    }
+                                    
+                                    
+                                }
+                               //print(self.list.count)
+                                print(self.categoryNameList.count)
+                                self.tableView.reloadData()
+                              
+                            })
+                           
+                        }
+                        
+                       // if let sections =
+                      
+                    }
+                    catch
+                    {
+                        print("Error procesando el json")
+                    }
+                    
+                }
+                
+            }
+            
+            
+            
+        }
+        task.resume()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,25 +111,57 @@ class GenresTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return list.count
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "genreCell", for: indexPath) as! GenresTableViewCellController
+        
+        let url = URL(string:list[indexPath.row].imgUrl)!
+        let request = NSMutableURLRequest(url:url)
+        let task = URLSession.shared.dataTask(with: request as URLRequest)
+        {
+            data, response, error in
+            if error != nil
+            {
+                print(error as Any)
+            }
+            else
+            {
+                if let dataUnwrapped = data
+                {
+                    if let imgGenre = UIImage(data: dataUnwrapped)
+                    {
+                        DispatchQueue.main.async {
+                            cell.genreImg.image = imgGenre
+                        }
+                       
+                    }
+                    
+                }
+                
+            }
+        }
+        task.resume()
+        cell.lblTitle.text = list[indexPath.row].genreName
+      
         return cell
     }
-    */
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedCategory = categoryNameList [indexPath.row]
+        performSegue(withIdentifier: "segueToStations", sender: nil)
+      
+        
+    }
+    
+    
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -55,17 +171,17 @@ class GenresTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    /*override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
-    }
-    */
+    }*/
+    
 
     /*
     // Override to support rearranging the table view.
