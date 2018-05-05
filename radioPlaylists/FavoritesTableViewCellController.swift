0627a3cf806b2a8fdcf03f343d5ade49ee3b536a
umadina <u.madina.i@gmail.com>
2018-05-05 20:12:08 +0200

    Added TableViewCellController for favorites view

---
+++ radioPlaylists/FavoritesTableViewCellController.swift
@@ -0,0 +1,24 @@
+//
+//  FavoritesTableViewCellController.swift
+//  radioPlaylists
+//
+//  Created by labdisca on 5/5/18.
+//  Copyright © 2018 umadina. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesTableViewCellController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
